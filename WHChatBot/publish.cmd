6fd5242e4bce7c082e8bf833ed378012db9e2766
Olevedeler <O9l5e567656>
2018-01-25 15:42:45 +0100

    Add project template

---
+++ WHChatBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=whchatbot-Web-Deploy.pubxml -p:Password=JjxLipWeHuegDfqEq4dXYtN5NCNjry8RSSWfn9ZDS8ug1xxFulnpB5nrtKPe
+
